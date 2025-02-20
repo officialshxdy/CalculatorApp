b766a12ac19bf812a4d181f84b7079858441a32b
officialshxdy <vandenbusscheshady@gmail.com>
2025-02-20 10:52:26 +0100

    Update

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            HStack{
+                Text("0").font(.largeTitle)
+            }
+            
+            Spacer()
+            VStack{
+                HStack{}
+            }
         }
         .padding()
     }
