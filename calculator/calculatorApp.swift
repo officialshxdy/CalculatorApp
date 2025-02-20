3ea89863d32badafa8ddabe19c26b219180f9295
officialshxdy <vandenbusscheshady@gmail.com>
2025-02-20 10:47:46 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Shady Vandenbussche on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
